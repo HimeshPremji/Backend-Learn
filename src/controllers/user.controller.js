c9979ac2811e2b24f5e57a6ea4c5db9088b6fc61
HimeshPremji <himeshdmaheswary@gmail.com>
2025-03-05 18:48:41 +0500

    Add refresh token functionality and improve JWT handling in user authentication

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,12 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const cookiesOptions = {
+  httpOnly: true,
+  secure: true,
+};
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -12,6 +18,8 @@ const generateAccessAndRefreshTokens = async (userId) => {
 
     user.refreshToken = refreshToken;
     await user.save({ validateBeforeSave: false });
+
+    console.log("accessToken", accessToken, "refreshToken", refreshToken);
 
     return { accessToken, refreshToken };
   } catch (error) {
@@ -93,8 +101,9 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
+  console.log(email);
 
-  if (!username || !email)
+  if (!username && !email)
     throw new ApiError(400, "username or email is required");
 
   const user = await User.findOne({
@@ -115,11 +124,6 @@ const loginUser = asyncHandler(async (req, res) => {
     "-password -refreshToken"
   );
 
-  const cookiesOptions = {
-    httpOnly: true,
-    secure: true,
-  };
-
   return res
     .status(200)
     .cookie("accessToken", accessToken, cookiesOptions)
@@ -138,26 +142,69 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $set: { refreshToken: undefined },
-    },
-    { new: true }
-  );
-
-  const cookiesOptions = {
-    httpOnly: true,
-    secure: true,
-  };
-
-  return res
-    .status(200)
-    .clearCookie(accessToken, cookiesOptions)
-    .clearCookie(
-      refreshToken,
-      cookiesOptions.json(new ApiResponse(200, {}, "User Logged Out"))
+  try {
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: { refreshToken: undefined },
+      },
+      { new: true }
     );
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", cookiesOptions)
+      .clearCookie("refreshToken", cookiesOptions)
+      .json(new ApiResponse(200, {}, "User Logged Out"));
+  } catch (error) {
+    throw new ApiError(404, "User is not logged in");
+  }
 });
 
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    // jwt tokens which are encrypted are inside incomingRefreshToken
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    // jwt tokens which are encrypted are inside user?>refreshToken
+    // if both tokens doesnt match it meants somethings wrong with the tokens
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh token is expired or used");
+    }
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, cookiesOptions)
+      .cookie("refreshToken", newRefreshToken, cookiesOptions)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Acccess token successfully refreshed"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token");
+  }
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
